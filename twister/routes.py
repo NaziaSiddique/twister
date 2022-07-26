01dea87803296b5d3d1efdd686f28f13fc82bdf0
Nazia Siddique <nazia.siddique04@gmail.com>
2022-07-26 15:55:37 +0000

    Fixed routes.py error and added base.html

--- twister/routes.py
+++ twister/routes.py
@@ -4,16 +4,16 @@ imports app and db from twister package
 """
 from flask import render_template
 from twister import app, db
-from twister.models import Category, Task
+#from twister.models import Category, Task
 
 
 @app.route("/")
 def home():
-"""
+    """
 to get the app running, create a basic app route
 using the root-level directory of slash
 this will be used to target the function, 'home'
 the function 'home' will return the rendered-template
 of 'base.html'
-"""
-return render_template("base.html")
+    """
+    return render_template("base.html")
