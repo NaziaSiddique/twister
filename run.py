73fa25cc9e879513ee624f11b3b191b5ded6327c
Nazia Siddique <nazia.siddique04@gmail.com>
2022-07-26 15:41:20 +0000

    Initial setup, WIP

---
+++ run.py
@@ -0,0 +1,14 @@
+"""
+imports os
+and imports app from twister folder
+"""
+import os
+from twister import app
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
---
+++ twister/routes.py
@@ -0,0 +1,19 @@
+"""
+imports render_template to use Flask functionality
+imports app and db from twister package
+"""
+from flask import render_template
+from twister import app, db
+from twister.models import Category, Task
+
+
+@app.route("/")
+def home():
+"""
+to get the app running, create a basic app route
+using the root-level directory of slash
+this will be used to target the function, 'home'
+the function 'home' will return the rendered-template
+of 'base.html'
+"""
+return render_template("base.html")
